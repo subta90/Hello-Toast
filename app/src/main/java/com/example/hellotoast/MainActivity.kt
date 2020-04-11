88a4ea653c9469316883204ae8149643eae85e60
subta90 <the.bird.nobility298@gmail.com>
2020-04-11 18:54:01 +0900

    finish

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText as makeText1
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+    private lateinit var mShowCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+        ++mCount
+        mShowCount.setText(Integer.toString(mCount))
     }
 }
